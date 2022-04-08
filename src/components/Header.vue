32980afae303b800bbbbea5f0f28fb5befde8259
Daniele Gelsomino <96696967+DanieleGelsomino@users.noreply.github.com>
2022-04-08 14:26:58 +0200

    add header

---
+++ src/components/Header.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="row icon-bar">
+    <div class="col-12">
+      <img class="" src="@/assets/spotify-logo.png" alt="" />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HeaderComponent",
+};
+</script>
+
+<style scoped lang="scss">
+@import "@/style/varstyle";
+
+.icon-bar {
+  background-color: $overlay-color;
+  img {
+    width: 45px;
+    margin-left: 5px;
+    padding: 5px;
+  }
+}
+</style>
